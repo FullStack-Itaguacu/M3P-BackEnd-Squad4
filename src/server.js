8f80a2972c23c5150e32048265b2dfa714e86e50
Leonardo Arena <leonardoarena7@gmail.com>
2023-10-03 09:09:27 -0300

    feat: configuração inicial do server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,38 @@
 const express = require("express");
+const cors = require("cors");
+const { connection } = require("./database/connection");
+const routes = require("./routes");
+
+class Server {
+  constructor(app = express()) {
+    this.initializeServer(app);
+    this.middlewares(app);
+    this.routes(app);
+    this.database();
+  }
+
+  async initializeServer(app) {
+    const port = 3333;
+    app.listen(port, () => console.log(`Servidor escutando na porta ${port}.`));
+  }
+
+  async middlewares(app) {
+    app.use(cors);
+    app.use(express.json());
+  }
+
+  async routes(app) {
+    app.use(routes);
+  }
+
+  async database() {
+    try {
+      await connection.authenticate();
+      console.log("Conexão com banco de dados bem sucedida.");
+    } catch (error) {
+      console.log("Não foi possível se conectar ao banco de dados.", error);
+    }
+  }
+}
+
+module.exports = { Server };
