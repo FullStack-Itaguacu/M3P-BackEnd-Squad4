0322b1e428da9c6075c5c374ed54a271ad90c4c6
Leonardo Arena <leonardoarena7@gmail.com>
2023-10-05 22:45:31 -0300

    fix: ajustes na migration addresses

--- src/database/migrations/20231004214856-create-table-addresses.js
+++ src/database/migrations/20231004214856-create-table-addresses.js
@@ -18,9 +18,9 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      numberStreet: {
+      number_street: {
         type: Sequelize.INTEGER,
-        allowNull: true,
+        allowNull: false,
       },
       neighborhood: {
         type: Sequelize.STRING,
@@ -46,15 +46,15 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: true,
       },
-      createdAt: {
+      created_at: {
         type: Sequelize.DATE,
         allowNull: false,
       },
-      updatedAt: {
+      updated_at: {
         type: Sequelize.DATE,
         allowNull: false,
       },
-      deletedAt: {
+      deleted_at: {
         type: Sequelize.DATE,
         allowNull: true,
       },
