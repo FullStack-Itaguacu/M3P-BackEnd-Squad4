3da811d8abc44b05823571d597e1a8c9c36107d6
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2023-10-11 06:30:24 -0300

    refactor: buyers.controller e buyers.routes

--- src/controller/buyers.controller.js
+++ src/controller/buyers.controller.js
@@ -1,6 +1,37 @@
-const { User } = require('../../models/user');
+const { Address } = require("../models/address");
+const { User } = require("../models/user");
 
-class BuyersAdmin {
+class BuyersController {
+  async listUserAddresses(req, res) {
+    try {
+      // Obtém o ID do usuário do payload do JWT
+      const userId = req.user && req.user.id;
+
+      // Verifica se o ID do usuário é válido
+      if (!userId || isNaN(userId) || userId <= 0) {
+        return res.status(400).send({
+          msg: "O ID do usuário deve ser um número válido",
+        });
+      }
+
+      const userAddresses = await Address.findAll({ where: { userId } });
+
+      if (!userAddresses || userAddresses.length === 0) {
+        return res.status(200).send({
+          msg: "O usuário não tem endereços cadastrados",
+        });
+      }
+
+      return res.status(200).send(userAddresses);
+    } catch (error) {
+      return res.status(401).send({
+        msg: "Erro ao processar a requisição",
+        error: error.message,
+      });
+    }
+  }
+
+  //admin
   async listAllBuyers(req, res) {
     try {
   
@@ -60,4 +91,4 @@ class BuyersAdmin {
   }
 }
 
-module.exports = new BuyersAdmin();
+module.exports = new BuyersController();
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,10 @@
 const { Router } = require("express");
-const { routesFromBuyerAddress } = require("./buyer.routes");
-const { routesFromBuyerAdmin } = require("./buyer.admin.routes");
+const { routesFromBuyers } = require("./buyers.routes");
 const routes = new Router();
 
 // routes.use();
 routes.use('/api', [
-   routesFromBuyerAddress(), routesFromBuyerAdmin()
+   routesFromBuyers()
 ]);
 
 
