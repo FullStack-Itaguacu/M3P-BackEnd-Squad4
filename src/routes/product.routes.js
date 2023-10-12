dd8a60b07170f523f3b222980e63d22f99c387bf
huitson-pereira <124617896+huitson-pereira@users.noreply.github.com>
2023-10-11 22:39:17 -0300

    feat: Implementação do arquivo: product.routes.js

---
+++ src/routes/product.routes.js
@@ -0,0 +1,28 @@
+const { Router } = require("express");
+const { 
+    createOneProduct, 
+    listAllProductsByUser, 
+    listAllProducts, 
+    listProductById, 
+    updateProductById
+} = require("../controller/product.controller");
+
+const { auth } = require("../middleware/auth");
+
+
+class ProductRouter {
+    routesFromProduct(){
+        const productRoutes = Router();
+
+        // Adicione a autenticação como middleware antes de cada rota
+        productRoutes.use(auth);
+
+        productRoutes.post('/products/admin', createOneProduct);
+        productRoutes.get('/products/admin/:offset/:limit', listAllProductsByUser);
+        productRoutes.get('/products/:offset/:limit', listAllProducts);
+        productRoutes.get('/products/:productId', listProductById);
+        productRoutes.patch('/products/admin/:productId', updateProductById)
+    }
+}
+
+module.exports = new ProductRouter();
