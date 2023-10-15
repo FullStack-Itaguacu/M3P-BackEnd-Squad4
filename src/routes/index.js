cd2f1a15e45217a58b5c2e53e08b74ac9e8991ed
Ana Paula <anapaulaprates8@gmail.com>
2023-10-15 18:48:52 -0300

    "Altera routes user no arquivo index.js"

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,30 +1,16 @@
 const { Router } = require("express");
-<<<<<<< HEAD
-const { routesFromBuyerAddress } = require("./buyer.routes");
 const { routesFromUser } = require("./user.routes");
-=======
-
-const { routesFromUser } = require("./user.routes");
-
 const { routesFromBuyer } = require("./buyer.routes");
 const { routesFromProduct } = require("./product.routes");
 
->>>>>>> a010438af5e453ab87a8e10e3af59c498e02d2ae
 const routes = new Router();
 
 
 routes.use('/api', [
-<<<<<<< HEAD
-   routesFromBuyerAddress(),
+
    routesFromUser(),
-=======
-
-   routesFromUser()
-
-
    routesFromBuyer(),
    routesFromProduct()
->>>>>>> a010438af5e453ab87a8e10e3af59c498e02d2ae
 
 ]);
 
