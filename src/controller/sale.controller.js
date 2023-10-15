c38646e1f2e3a0b1e098856d69dc869253e15eff
Cleyton S. Vitor <cleyton.svitor@gmail.com>
2023-10-15 19:03:02 -0300

    "add controller e route for sale"

---
+++ src/controller/sale.controller.js
@@ -0,0 +1,39 @@
+const { User } = require('../models/user');
+const{ Sale } = require('../models/sales');
+const { Product } = require('../models/product');
+
+
+class SaleController {
+    async createOneSale(req, res) {
+        try {
+
+        } catch (error) {
+
+        }
+    }
+    async listSale(req, res) {
+        try {
+
+        } catch (error) {
+
+        }
+    }
+
+    async listSaleByAdmin(req, res) {
+        try {
+
+        } catch (error) {
+
+        }
+    }
+    async listResultByAdmin(req, res) {
+        try {
+
+        } catch (error) {
+
+        }
+    }
+
+}
+
+module.exports = new SaleController();
---
+++ src/routes/sale.routes.js
@@ -0,0 +1,19 @@
+const { Router } = require("express");
+const { createOneSale, listSale, listSaleByAdmin, listResultByAdmin } = require("../controller/sale.controller");
+const { auth } = require("../middleware/auth");
+
+
+class SaleRouter {
+    routesFromSale(){
+        const saleRoutes = Router();
+        saleRoutes.use(auth);
+        saleRoutes.post('/sales/', createOneSale);
+        saleRoutes.get('/sales/', listSale);
+        saleRoutes.get('/sales/admin', listSaleByAdmin);
+        saleRoutes.get('/sales/admin', listResultByAdmin);
+       
+        return saleRoutes;
+    }
+}
+
+module.exports = new SaleRouter();
