9dcbf3df3d3f69f4abeb92e9c565704f28823406
Ana Paula <anapaulaprates8@gmail.com>
2023-10-15 18:03:09 -0300

    cria controller login/admin/user

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -7,48 +7,24 @@ class UserController {
   async loginUser(req, res) {
     try {
       const { email, password } = req.body;
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> a010438af5e453ab87a8e10e3af59c498e02d2ae
       // Verifica se o e-mail e a senha foram fornecidos
       if (!email || !password) {
         return res.status(401).json({ error: "E-mail e senha são campos obrigatórios." });
       }
-<<<<<<< HEAD
-
-=======
-  
->>>>>>> a010438af5e453ab87a8e10e3af59c498e02d2ae
       // Busca o usuário no banco de dados pelo e-mail
       const user = await User.findOne({
         where: { email },
       });
-<<<<<<< HEAD
-
-=======
-  
->>>>>>> a010438af5e453ab87a8e10e3af59c498e02d2ae
       // Verifica se o usuário foi encontrado
       if (!user) {
         return res.status(401).json({ error: "E-mail incorreto." });
       }
-<<<<<<< HEAD
-
-=======
-  
->>>>>>> a010438af5e453ab87a8e10e3af59c498e02d2ae
       // Verifica se a senha está correta
       const passwordMatch = await bcrypt.compare(password, user.password);
       if (!passwordMatch) {
         return res.status(401).json({ error: "Senha incorreta." });
       }
-<<<<<<< HEAD
-
-=======
-  
->>>>>>> a010438af5e453ab87a8e10e3af59c498e02d2ae
       // Cria um token JWT com os campos solicitados no payload
       const tokenPayload = {
         id: user.id,
@@ -56,25 +32,19 @@ class UserController {
         full_name: user.fullName,
         type_user: user.typeUser,
       };
-<<<<<<< HEAD
-
-=======
-  
->>>>>>> a010438af5e453ab87a8e10e3af59c498e02d2ae
       const secretKey = process.env.SECRET_KEY_BUYER;
 
       // Gera um token para o usuário
       const token = jwt.sign(tokenPayload, secretKey);
-<<<<<<< HEAD
 
-
-      // Retorna o token no corpo da resposta
+     // Retorna o token no corpo da resposta
       res.status(200).json({ token });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: "Erro interno do servidor." });
     }
   }
+  
   async loginAdminUser(req, res) {
     try {
       const { email, password } = req.body;
@@ -114,10 +84,6 @@ class UserController {
       const token = jwt.sign(tokenPayload, secretKey);
 
 
-=======
-      
-  
->>>>>>> a010438af5e453ab87a8e10e3af59c498e02d2ae
       // Retorna o token no corpo da resposta
       res.status(200).json({ token });
     } catch (error) {
