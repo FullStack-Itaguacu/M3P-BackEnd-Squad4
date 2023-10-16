fd90eee6ad9a7b819c9723c6c65d913d8be1abbd
Cleyton S. Vitor <cleyton.svitor@gmail.com>
2023-10-16 20:31:16 -0300

    "feat: add controller and routes from sale"

--- src/controller/sale.controller.js
+++ src/controller/sale.controller.js
@@ -1,19 +1,43 @@
 const { User } = require('../models/user');
-const{ Sale } = require('../models/sales');
+const { Sale } = require('../models/sales');
 const { Product } = require('../models/product');
 
 
 class SaleController {
     async createOneSale(req, res) {
         try {
+            const saleData = req.body;
+            // Itere sobre cada objeto no vetor
+            for (const saleItem of saleData) {
+
+                const newSale = Sale.build({
+                    product_id: saleItem.product_id,
+                    amount_buy: saleItem.amount_buy,
+                    users_addresses_id: saleItem.users_addresses_id,
+                    type_payment: saleItem.type_payment,
+                    unit_price: 1.00,
+                })
+
+                await newSale.save();
+               
+            }
+
+            res.status(201).json({ msg: 'Venda criada com sucesso' });
+
 
         } catch (error) {
+
+            console.error('Erro no controller de venda:', error);
+            res.status(500).json({
+                error:
+                    { msg: 'Erro ao processar a requisição', details: error.message }
+            });
 
         }
     }
     async listSale(req, res) {
         try {
-
+            console.log("cheguei no listSale")
         } catch (error) {
 
         }
--- src/routes/sale.routes.js
+++ src/routes/sale.routes.js
@@ -1,12 +1,12 @@
 const { Router } = require("express");
 const { createOneSale, listSale, listSaleByAdmin, listResultByAdmin } = require("../controller/sale.controller");
-const { auth } = require("../middleware/auth");
+//const { auth } = require("../middleware/auth");
 
 
 class SaleRouter {
     routesFromSale(){
         const saleRoutes = Router();
-        saleRoutes.use(auth);
+        //saleRoutes.use(auth);
         saleRoutes.post('/sales/', createOneSale);
         saleRoutes.get('/sales/', listSale);
         saleRoutes.get('/sales/admin', listSaleByAdmin);
