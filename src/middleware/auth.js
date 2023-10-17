b074384cc020c66240e9fd2b09dbf440d9c11084
Leonardo Arena <leonardoarena7@gmail.com>
2023-10-16 22:01:23 -0300

    feat: melhoria no middleware auth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,18 +1,17 @@
-const { verify } = require('jsonwebtoken')
-const dotenv = require('dotenv');
-dotenv.config();
+const { verify } = require("jsonwebtoken");
+const { jwt_secret_key } = require("../config/database.config");
 
-async function auth(request, response, next){
-    try {
-        console.log(request)
-        const { authorization } = request.headers
-        request["payload"] = verify(authorization, process.env.JWT_SECRET_KEY)
-        next()
-    } catch (error) {
-        return response.status(401).send({
-            message: "Falha na autenticação",
-            cause: error.message})
-    }
+async function auth(request, response, next) {
+  try {
+    const { authorization } = request.headers;
+    request["payload"] = verify(authorization, jwt_secret_key);
+    next();
+  } catch (error) {
+    return response.status(401).send({
+      message: "Falha na autenticação",
+      cause: error.message,
+    });
+  }
 }
 
-module.exports = { auth }
+module.exports = { auth };
