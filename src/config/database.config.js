f2fd8e968571688eb1bda1d46b047bfb3ef341f5
Leonardo Arena <leonardoarena7@gmail.com>
2023-10-16 21:58:55 -0300

    feat: melhoria na jwt secret key

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,7 +1,7 @@
 const { config } = require("dotenv");
 config();
 
-const { DIALECT, HOST, USERNAMEDB, PASSWORDDB, DATABASE, PORT } = process.env;
+const { DIALECT, HOST, USERNAMEDB, PASSWORDDB, DATABASE, PORT, JWT_SECRET_KEY } = process.env;
 
 module.exports = {
   dialect: DIALECT,
@@ -10,6 +10,7 @@ module.exports = {
   password: PASSWORDDB,
   database: DATABASE,
   port: PORT,
+  jwt_secret_key: JWT_SECRET_KEY,
   define: {
     underscored: true,
     underscoredAll: true,
