5063eb94c935a5847a837ece432748b106470dd8
Ana Paula <anapaulaprates8@gmail.com>
2023-10-17 23:33:12 -0300

    "refactor: Melhorias na UserController"

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -4,256 +4,314 @@ const bcrypt = require('bcrypt');
 const { Op } = require('sequelize');
 const { sign } = require('jsonwebtoken');
 const { jwt_secret_key } = require('../config/database.config');
+const { throwErrorIf, validatePassword, validatePhone, validateCPF } = require('../services/user.services')
 
 class UserController {
 
-    async loginUser(req, res) {
-        try {
-            const { email, password } = req.body;
+  async loginUser(req, res) {
+    try {
+      const { email, password } = req.body;
 
-            // Verifica se o e-mail e a senha foram fornecidos
-            if (!email || !password) {
-                return res.status(401).json({ error: "E-mail e senha são campos obrigatórios." });
-            }
+      // Verifica se o e-mail e a senha foram fornecidos
+      if (!email || !password) {
+        return res.status(401).json({ error: "E-mail e senha são campos obrigatórios." });
+      }
 
-            // Busca o usuário no banco de dados pelo e-mail
-            const user = await User.findOne({
-                where: { email },
-            });
+      // Busca o usuário no banco de dados pelo e-mail
+      const user = await User.findOne({
+        where: { email },
+      });
 
-            // Verifica se o usuário foi encontrado
-            if (!user) {
-                return res.status(401).json({ error: "E-mail incorreto." });
-            }
+      // Verifica se o usuário foi encontrado
+      if (!user) {
+        return res.status(401).json({ error: "E-mail incorreto." });
+      }
 
-            // Verifica se a senha está correta
-            const passwordMatch = await bcrypt.compare(password, user.password);
-            if (!passwordMatch) {
-                return res.status(401).json({ error: "Senha incorreta." });
-            }
+      // Verifica se a senha está correta
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (!passwordMatch) {
+        return res.status(401).json({ error: "Senha incorreta." });
+      }
 
-            // Cria um token JWT com os campos solicitados no payload
-            const tokenPayload = {
-                id: user.id,
-                email: user.email,
-                full_name: user.fullName,
-                type_user: user.typeUser,
-            };
+      // Cria um token JWT com os campos solicitados no payload
+      const tokenPayload = {
+        id: user.id,
+        email: user.email,
+        full_name: user.fullName,
+        type_user: user.typeUser,
+      };
 
-            // Gera um token para o usuário
-            const token = sign(tokenPayload, jwt_secret_key);
+      // Gera um token para o usuário
+      const token = sign(tokenPayload, jwt_secret_key, { expiresIn: "1d" });
 
-            // Retorna o token no corpo da resposta
-            res.status(200).json({ token });
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: "Erro interno do servidor." });
-        }
+      // Retorna o token no corpo da resposta
+      res.status(200).json({ token });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Erro interno do servidor." });
     }
-    async loginAdminUser(req, res) {
-        try {
-            const { email, password } = req.body;
+  }
 
-            // Verifique se o e-mail e a senha foram fornecidos
-            if (!email || !password) {
-                return res.status(401).json({ error: "E-mail e senha são campos obrigatórios." });
-            }
+  async loginAdminUser(req, res) {
+    try {
+      const { email, password } = req.body;
 
-            // Busque o usuário no banco de dados pelo e-mail
-            const user = await User.findOne({
-                where: { email },
-            });
+      // Verifica se o e-mail e a senha foram fornecidos
+      if (!email || !password) {
+        return res.status(401).json({ error: "E-mail e senha são campos obrigatórios." });
+      }
 
-            // Verifique se o usuário foi encontrado
-            if (!user) {
-                return res.status(401).json({ error: "E-mail incorreto." });
-            }
+      // Busca o usuário no banco de dados pelo e-mail
+      const user = await User.findOne({
+        where: { email },
+      });
 
-            // Verifique se a senha está correta
-            const passwordMatch = await bcrypt.compare(password, user.password);
-            if (!passwordMatch) {
-                return res.status(401).json({ error: "Senha incorreta." });
-            }
+      // Verifica se o usuário foi encontrado
+      if (!user) {
+        return res.status(401).json({ error: "E-mail incorreto." });
+      }
 
-            // Crie um token JWT com os campos solicitados no payload
-            const tokenPayload = {
-                id: user.id,
-                email: user.email,
-                full_name: user.fullName,
-                type_user: user.typeUser,
-            };
+      // Verifica se a senha está correta
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (!passwordMatch) {
+        return res.status(401).json({ error: "Senha incorreta." });
+      }
 
-            // Gere um token para o usuário
-            const token = sign(tokenPayload, jwt_secret_key);
+      // Cria um token JWT com os campos solicitados no payload
+      const tokenPayload = {
+        id: user.id,
+        email: user.email,
+        full_name: user.fullName,
+        type_user: user.typeUser,
+      };
 
-            // Retorne o token no corpo da resposta
-            res.status(200).json({ token });
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: "Erro interno do servidor." });
-        }
+      // Gera um token para o usuário
+      const token = sign(tokenPayload, jwt_secret_key, { expiresIn: "1d" });
+
+      // Retorna o token no corpo da resposta
+      res.status(200).json({ token });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Erro interno do servidor." });
     }
-    async buyerSignup(req, res) {
-        try {
-          const { user, address } = req.body;
-    
-          const {
-            fullName,
-            cpf,
-            birthDate,
-            email,
-            phone,
-            password,
-          } = user;
-    
-          const {
-            zip,
-            street,
-            numberStreet,
-            neighborhood,
-            city,
-            state,
-            complement,
-            lat,
-            long,
-          } = address;
-    
-          // Verifica campos obrigatórios
-          if ([
-            fullName, cpf, birthDate, email, phone, password, zip, street,
-            numberStreet, neighborhood, city, state
-          ].some(field => !field)) {
-            return res.status(422).json({ message: 'Campos obrigatórios não preenchidos.' });
-          }
-    
-          // Valida Telefone
-          if (!/^\d{9,15}$/.test(phone)) {
-            return res.status(400).json({ message: 'Telefone inválido.' });
-          }
-    
-          // Cria o endereço.
-          const createdAddress = await Address.create({
-            zip,
-            street,
-            numberStreet,
-            neighborhood,
-            city,
-            state,
-            complement,
-            lat,
-            long,
+  }
+
+  async buyerSignup(req, res) {
+    try {
+      const { user, address } = req.body;
+
+      const {
+        fullName,
+        cpf,
+        birthDate,
+        email,
+        phone,
+        password,
+      } = user;
+
+      const {
+        zip,
+        street,
+        numberStreet,
+        neighborhood,
+        city,
+        state,
+        complement,
+        lat,
+        long,
+      } = address;
+
+      // Verifica campos obrigatórios
+      throwErrorIf(!fullName, "O campo nome é obrigatório.");
+      throwErrorIf(!cpf, "O campo CPF é obrigatório.");
+      throwErrorIf(!birthDate, "O campo data de nascimento é obrigatório.");
+      throwErrorIf(!email, "O campo email é obrigatório.");
+      throwErrorIf(!phone, "O campo telefone é obrigatório.");
+      throwErrorIf(!password, "O campo senha é obrigatório.");
+      throwErrorIf(!zip, "O campo cep é obrigatório.");
+      throwErrorIf(!street, "O campo logradouro é obrigatório.");
+      throwErrorIf(!numberStreet, "O campo número é obrigatório.");
+      throwErrorIf(!neighborhood, "O campo bairro é obrigatório.");
+      throwErrorIf(!city, "O campo cidade é obrigatório.");
+      throwErrorIf(!state, "O campo estado é obrigatório.");
+
+      // Validação da senha
+      const passwordValidation = validatePassword(user.password);
+      if (passwordValidation) {
+        return res.status(passwordValidation.status).json({ message: passwordValidation.message });
+      }
+
+      // Validação do telefone
+      const phoneValidation = validatePhone(user.phone);
+      if (phoneValidation) {
+        return res.status(phoneValidation.status).json({ message: phoneValidation.message });
+      }
+
+      // Validação do CPF
+      const cpfValidation = validateCPF(user.cpf);
+      if (cpfValidation) {
+        return res.status(cpfValidation.status).json({ message: cpfValidation.message });
+      }
+
+      // Cria o endereço.
+      const createdAddress = await Address.create({
+        zip,
+        street,
+        numberStreet,
+        neighborhood,
+        city,
+        state,
+        complement,
+        lat,
+        long,
+      });
+
+      // Cria o usuário.
+      const hashedPassword = await bcrypt.hash(password, 10); // Criptografe a senha.
+      const createdUser = await User.create({
+        fullName,
+        cpf,
+        birthDate,
+        email,
+        phone,
+        password: hashedPassword,
+        addressId: createdAddress.id,
+        typeUser: 'Comprador',
+      });
+
+      return res.status(201).send({ message: "Usuário cadastrado com sucesso." });
+    } catch (error) {
+      if (error.name === "SequelizeUniqueConstraintError") {
+        if (error.fields.email || error.fields.cpf) {
+          return res.status(409).send({
+            message: "Não foi possível cadastrar o usuário.",
+            cause: error.message,
           });
-    
-          // Cria o usuário.
-          const hashedPassword = await bcrypt.hash(password, 10); // Criptografe a senha.
-          const createdUser = await User.create({
-            fullName,
-            cpf,
-            birthDate,
-            email,
-            phone,
-            password: hashedPassword,
-            addressId: createdAddress.id,
-            typeUser: 'Comprador',
-          });
-    
-          return res.status(201).json({ message: 'Registros criados com sucesso.' });
-        } catch (error) {
-          if (error.name === 'SequelizeUniqueConstraintError') {
-            return res.status(409).json({ message: 'CPF ou email já cadastrados.' });
-          }
-          console.error(error);
-          return res.status(400).json({ message: 'Erro ao criar registros.' });
         }
       }
-      async adminSignup(req, res) {
-        try {
-          const { user, address } = req.body;
-    
-          const {
-            fullName,
-            cpf,
-            birthDate,
-            email,
-            phone,
-            password,
-            typeUser,
-          } = user;
-    
-          const {
-            zip,
-            street,
-            numberStreet,
-            neighborhood,
-            city,
-            state,
-            complement,
-            lat,
-            long,
-          } = address;
-    
-          // Verifica campos obrigatórios
-          if ([
-            fullName, cpf, birthDate, email, phone, password, typeUser, zip, street,
-            numberStreet, neighborhood, city, state
-          ].some(field => !field)) {
-            return res.status(422).json({ message: 'Campos obrigatórios não preenchidos.' });
-          }
-    
-          // Valida Telefone
-          if (!/^\d{9,15}$/.test(phone)) {
-            return res.status(400).json({ message: 'Telefone inválido.' });
-          }
-    
-          // Verifica se já existe um usuário com o mesmo CPF ou e-mail
-          const existingUser = await User.findOne({
-            where: {
-              [Op.or]: [
-                { cpf: user.cpf },
-                { email: user.email }
-              ]
-            }
+
+      return res.status(400).send({
+        message: "Não foi possível cadastrar o usuário.",
+        cause: error.message,
+      });
+    }
+  }
+
+  async adminSignup(req, res) {
+    try {
+      const { user, address } = req.body;
+
+      const {
+        fullName,
+        cpf,
+        birthDate,
+        email,
+        phone,
+        password,
+        typeUser,
+      } = user;
+
+      const {
+        zip,
+        street,
+        numberStreet,
+        neighborhood,
+        city,
+        state,
+        complement,
+        lat,
+        long,
+      } = address;
+
+      // Verifica campos obrigatórios
+      throwErrorIf(!fullName, "O campo nome é obrigatório.");
+      throwErrorIf(!cpf, "O campo CPF é obrigatório.");
+      throwErrorIf(!birthDate, "O campo data de nascimento é obrigatório.");
+      throwErrorIf(!email, "O campo email é obrigatório.");
+      throwErrorIf(!phone, "O campo telefone é obrigatório.");
+      throwErrorIf(!password, "O campo senha é obrigatório.");
+      throwErrorIf(!zip, "O campo cep é obrigatório.");
+      throwErrorIf(!street, "O campo logradouro é obrigatório.");
+      throwErrorIf(!numberStreet, "O campo número é obrigatório.");
+      throwErrorIf(!neighborhood, "O campo bairro é obrigatório.");
+      throwErrorIf(!city, "O campo cidade é obrigatório.");
+      throwErrorIf(!state, "O campo estado é obrigatório.");
+      throwErrorIf(!typeUser, "O campo tipo de usuário é obrigatório.");
+
+      // Validação da senha
+      const passwordValidation = validatePassword(user.password);
+      if (passwordValidation) {
+        return res.status(passwordValidation.status).json({ message: passwordValidation.message });
+      }
+
+      // Validação do telefone
+      const phoneValidation = validatePhone(user.phone);
+      if (phoneValidation) {
+        return res.status(phoneValidation.status).json({ message: phoneValidation.message });
+      }
+
+      // Validação do CPF
+      const cpfValidation = validateCPF(user.cpf);
+      if (cpfValidation) {
+        return res.status(cpfValidation.status).json({ message: cpfValidation.message });
+      }
+
+      // Verifica se já existe um usuário com o mesmo CPF ou e-mail
+      const existingUser = await User.findOne({
+        where: {
+          [Op.or]: [
+            { cpf: user.cpf },
+            { email: user.email }
+          ]
+        }
+      });
+
+      if (existingUser) {
+        return res.status(409).json({ message: "Usuário com CPF ou e-mail já cadastrado." });
+      }
+
+      // Cria o endereço.
+      const createdAddress = await Address.create({
+        zip,
+        street,
+        numberStreet,
+        neighborhood,
+        city,
+        state,
+        complement,
+        lat,
+        long,
+      });
+
+      // Cria o usuário.
+      const hashedPassword = await bcrypt.hash(password, 10); // Criptografa a senha.
+      const createdUser = await User.create({
+        fullName,
+        cpf,
+        birthDate,
+        email,
+        phone,
+        password: hashedPassword,
+        addressId: createdAddress.id,
+        typeUser: 'Administrador',
+      });
+
+      return res.status(201).send({ message: "Usuário cadastrado com sucesso." });
+    } catch (error) {
+      if (error.name === "SequelizeUniqueConstraintError") {
+        if (error.fields.email || error.fields.cpf) {
+          return res.status(409).send({
+            message: "Não foi possível cadastrar o usuário.",
+            cause: error.message,
           });
-    
-          if (existingUser) {
-            return res.status(409).json({ message: "Usuário com CPF ou e-mail já cadastrado." });
-          }
-    
-          // Cria o endereço.
-          const createdAddress = await Address.create({
-            zip,
-            street,
-            numberStreet,
-            neighborhood,
-            city,
-            state,
-            complement,
-            lat,
-            long,
-          });
-    
-          // Cria o usuário.
-          const hashedPassword = await bcrypt.hash(password, 10); // Criptografa a senha.
-          const createdUser = await User.create({
-            fullName,
-            cpf,
-            birthDate,
-            email,
-            phone,
-            password: hashedPassword,
-            addressId: createdAddress.id,
-            typeUser: 'Administrador',
-          });
-    
-          return res.status(201).json({ message: 'Registros criados com sucesso.' });
-        } catch (error) {
-          if (error.name === 'SequelizeUniqueConstraintError') {
-            return res.status(409).json({ message: 'CPF ou email já cadastrados.' });
-          }
-          console.error(error);
-          return res.status(400).json({ message: 'Erro ao criar registros.' });
         }
       }
+      return res.status(422).send({
+        message: "Não foi possível cadastrar o usuário.",
+        cause: error.message,
+      });
+    }
+  }
 }
 
 module.exports = new UserController();
