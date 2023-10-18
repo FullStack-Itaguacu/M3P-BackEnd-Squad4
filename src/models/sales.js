e3aa1596075db8404950b15993d480a552559d84
Leonardo Arena <leonardoarena7@gmail.com>
2023-10-18 20:09:14 -0300

    fix: ajustes na model sale

--- src/models/sales.js
+++ src/models/sales.js
@@ -1,78 +1,98 @@
 const { connection } = require("../database/connection");
-const { INTEGER, REAL, ENUM, DATE } = require("sequelize")
+const { INTEGER, REAL, ENUM, DATE } = require("sequelize");
 
 const Sale = connection.define(
-    "sale",
-    {
-        id: {
-            allowNull: false,
-            type: INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        /* buyer_id: {
-            type: INTEGER,
-            allowNull: false,
-        }, */
-        /* seller_id: {
-            type: INTEGER,
-            allowNull: false,
-        }, */
-       /*  product_id: {
-            type: INTEGER,
-            allowNull: false,
-        }, */
-        unit_price: {
-            type: REAL,
-            allowNull: false,
-        },
-        amount_buy: {
-            type: INTEGER,
-            allowNull: false,
-        },
-        /* users_addresses_id: {
-            type: INTEGER,
-            allowNull: false,
-        }, */
-        created_at: {
-            type: DATE,
-            allowNull: false,
-        },
-        update_at: {
-            type: DATE,
-            allowNull: false,
-        },
-        total: {
-            type: REAL,
-            allowNull: false,
-        },
-        type_payment: {
-            type: ENUM(
-                'cartão de crédito',
-                'cartão de débito',
-                'PIX', 'boleto',
-                'transferência bancária'
-            ),
-            allowNull: false,
-        },
+  "sale",
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    { underscored: true, paranoid: true }
+    unit_price: {
+      type: REAL,
+      allowNull: false,
+    },
+    amount_buy: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    total: {
+      type: REAL,
+      allowNull: false,
+    },
+    type_payment: {
+      type: ENUM(
+        "cartão de crédito",
+        "cartão de débito",
+        "PIX",
+        "boleto",
+        "transferência bancária"
+      ),
+      allowNull: false,
+    },
+    buyer_id: {
+      type: INTEGER,
+      allowNull: false,
+      references: {
+        model: { tableName: "users" },
+        key: "id",
+      },
+    },
+    seller_id: {
+      type: INTEGER,
+      allowNull: false,
+      references: {
+        model: { tableName: "users" },
+        key: "id",
+      },
+    },
+    product_id: {
+      type: INTEGER,
+      allowNull: false,
+      references: {
+        model: { tableName: "products" },
+        key: "id",
+      },
+    },
+    users_addresses_id: {
+      type: INTEGER,
+      allowNull: false,
+      references: {
+        model: { tableName: "users_addresses" },
+        key: "id",
+      },
+    },
+    created_at: {
+      type: DATE,
+      allowNull: false,
+    },
+    update_at: {
+      type: DATE,
+      allowNull: false,
+    },
+  },
+  { underscored: true, paranoid: true }
 );
 
-Sale.associate = (models) => {
-    Sale.hasMany(models.User, { foreignKey: 'buyer_id', as: 'users' });
-};
+// Sale.associate = (models) => {
+//   Sale.hasMany(models.User, { foreignKey: "buyer_id", as: "users" });
+// };
 
-Sale.associate = (models) => {
-    Sale.hasMany(models.User, { foreignKey: 'seller_id', as: 'users' });
-};
+// Sale.associate = (models) => {
+//   Sale.hasMany(models.User, { foreignKey: "seller_id", as: "users" });
+// };
 
-Sale.associate = (models) => {
-    Sale.hasMany(models.Product, { foreignKey: 'product_id', as: 'produtcs' });
-};
+// Sale.associate = (models) => {
+//   Sale.hasMany(models.Product, { foreignKey: "product_id", as: "produtcs" });
+// };
 
-Sale.associate = (models) => {
-    Sale.hasMany(models.address, { foreignKey: 'users_addresses_id', as: 'addresses' });
-};
+// Sale.associate = (models) => {
+//   Sale.hasMany(models.address, {
+//     foreignKey: "users_addresses_id",
+//     as: "addresses",
+//   });
+// };
 
 module.exports = { Sale };
