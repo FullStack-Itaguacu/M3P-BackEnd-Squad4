f242c56445a3e05d86ee111b72524417c319da12
Leonardo Arena <leonardoarena7@gmail.com>
2023-10-28 19:52:39 -0300

    fix: ajuste na função validatePassword

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,5 +1,5 @@
 const validatePassword = (password) => {
-    if (!/(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[a-zA-Z\d@$!%*?&]{8,}/.test(password)) {
+    if (!/(?=.*[A-Z])(?=.*\d)(?=.*[#@$!%*?&])[a-zA-Z\d#@$!%*?&]{8,}/.test(password)) {
       return {
         status: 400,
         message: 'A senha deve ter no mínimo 8 caracteres, sendo pelo menos 1 letra maiúscula, 1 número e 1 caractere especial.',
